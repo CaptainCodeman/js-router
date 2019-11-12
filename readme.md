a7cc9d84b95ca583a41dc403581a8282bc61e80e
Simon Green <simon@captaincodeman.com>
2019-11-12 10:41:28 -0700

    Prep for publishing

---
+++ readme.md
@@ -0,0 +1,7 @@
+## Router
+
+[![minzipped size](https://badgen.net/bundlephobia/minzip/@captaincodeman/router)]
+
+An ultra-tiny client-side router for modern pregressive web apps.
+
+"ultra-tiny" = 537 bytes minified, 367 bytes minified and gzipped.
--- rollup.config.js
+++ rollup.config.js
@@ -1,8 +1,9 @@
 'use strict';
 
+import pkg from './package.json';
+
 import typescript from 'rollup-plugin-typescript';
 import { terser } from 'rollup-plugin-terser';
-import size from 'rollup-plugin-size';
 
 // assume production mode for normal build, dev if watched
 // flag is used to enable / disable HTML & JS minification
@@ -11,21 +12,13 @@ const production = !process.env.ROLLUP_WATCH;
 export default {
   input: 'src/router.ts',
   output: {
-    file: 'dist/router.min.js',
+    file: 'dist/' + pkg.main,
     format: 'esm',
     name: 'router',
     sourcemap: false,
   },
   plugins: [
     typescript({ typescript: require('typescript') }),
-    production && terser({
-      ecma: 6,
-      module: true,
-      compress: {
-        pure_getters: true,
-        unsafe: true,
-      },
-    }),
-    production && size(),
+    production && terser(),
   ],
 }
